95bee0903a095da1e768098658190d920c4b348f
bohaoi <bohao8485@163.com>
2020-09-01 10:07:05 +0800

    fastclick

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
+//1.引入fastclick
+import FastClick from "fastclick"
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function() {
+      FastClick.attach(document.body);
+  }, false);
+}
+
 Vue.config.productionTip = false
 
 new Vue({

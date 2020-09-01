00838425fc43c935921dbaa6b63c768d534af8bb
bohaoi <bohao8485@163.com>
2020-09-01 10:33:44 +0800

    views

---
+++ src/views/cart/Cart.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="cart">购物车</div></div>
+</template>
+
+<script>
+export default {
+  name: "Cart",
+};
+</script>
+
+<style lang="less" scoped>
+#cart {
+  width: 100%;
+  height: 100%;
+  background-color: green;
+}
+</style>
---
+++ src/views/dashboard/DashBoard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="dashboard">分类</div>
+</template>
+
+<script>
+export default {
+  name: "DashBoard",
+};
+</script>
+
+<style lang="less" scoped>
+#dashboard {
+  width: 100%;
+  height: 100%;
+  background-color: gold;
+}
+</style>

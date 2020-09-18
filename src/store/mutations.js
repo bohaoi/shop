5db1f00a6def91d6d9a119c4eac5032b6be7762e
bohaoi <bohao8485@163.com>
2020-09-18 11:40:27 +0800

    add cart

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,4 +1,6 @@
-import {ADD_GOODS} from './mutations-type'
+import {ADD_GOODS,INIT_SHOP_CART} from './mutations-type'
+
+import {getStore,removeStore,setStore} from './../config/global'
 
 export default{
     //1. 往购物车中添加数据
@@ -19,5 +21,14 @@ export default{
         }
         //1.2 产生新对象
         state.shopCart = {...shopCart}
+        //1.3 本地存储
+        setStore('shopCart',state.shopCart)
+    },
+    //2. 页面初始化，获取购物车的数据(本地)
+    [INIT_SHOP_CART](state){
+        let initCart = getStore("shopCart");
+        if(initCart){
+            state.shopCart = JSON.parse(initCart)
+        }
     }
 }

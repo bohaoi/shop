411cedbd37e853a01fee6b722d9e267c4713c07e
bohaoi <bohao8485@163.com>
2020-11-23 11:14:34 +0800

    change

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -5,7 +5,7 @@ import ajax from "./ajax"
 // http://demo.itlike.com/web/xlmc/api/homeApi/categoriesdetail/lk001
 
 //1. 定义基础路径
-const BASE_URL = 'http://demo.itlike.com/web/xlmc';
+const BASE_URL = 'https://demo.itlike.com/web/xlmc';
 
 export const getHomeData = () => ajax(BASE_URL + '/api/homeApi');
 export const getCategories = () => ajax(BASE_URL + '/api/homeApi/categories');
@@ -13,7 +13,7 @@ export const getCategoriesDetail = (preParams) => ajax(BASE_URL + '/api/homeApi/
 
 // 2. 用户中心接口
 //  http://localhost:3000/web/xlmc/api/send_code
-const LOCAL_BASE_URL = 'http://demo.itlike.com/web/xlmc';
+const LOCAL_BASE_URL = 'https://demo.itlike.com/web/xlmc';
 
 // 2.1 获取短信验证码(GET)
 export const getPhoneCode = (phone) => ajax(LOCAL_BASE_URL + '/api/send_code', {phone});
